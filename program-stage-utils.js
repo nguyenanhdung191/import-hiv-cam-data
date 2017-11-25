bbe75f2c6bb64b3ff9906317c68926d8eb0130b2
nghia <nghianguyentran92@gmail.com>
2017-11-25 12:46:11 +0700

    nghia update program-stage-utils.js version 1

---
+++ program-stage-utils.js
@@ -0,0 +1,56 @@
+let XLSX = require('xlsx');
+const jsonwrite = require("jsonfile");
+let workbook = XLSX.readFile(`./.xlsx`);
+const excelContent = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+
+
+let event = {};
+event["events"] = [];
+
+const generateEventsNghia = (data) => {
+    excelContent.forEach(exc => {
+
+        event["events"].push(createEventObject(exc,data));
+        createJsonFile();
+
+    });
+}
+
+
+
+const createJsonFile = () => {
+    jsonwrite.writeFileSync(`./AfterUpdate.json`, event);
+}
+
+const createEventObject = (exc, data) => {
+
+    let temp;
+    if (data.hasOwnProperty(exc.PatientID)) {
+        temp = {
+            "programStage": "",
+            "orgUnit": "",
+            "program": "",
+            "trackedEntityInstance": data[exc.PatientID].trackedEntityInstance,
+            "enrollment": data[exc.PatientID].Enrollment,
+            "status": "COMPLETED",
+            "eventDate": "",
+            "followup": true,
+            "dataValues": []
+        }
+
+        for (z in exc) {
+            const dataval = {
+                "dataElement": z,
+                "value": exc[z]
+            };
+            temp.dataValues.push(dataval);
+        }
+
+        return temp;
+    }
+
+}
+
+modules.export = {
+    generateEventsNghia
+}
