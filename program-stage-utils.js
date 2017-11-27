cfb1e40e51c8155fa5fb48adcaec816c8785e45a
nghia <nghianguyentran92@gmail.com>
2017-11-27 09:28:27 +0700

    update program-stage-utils.js version 3

--- program-stage-utils.js
+++ program-stage-utils.js
@@ -1,56 +1,72 @@
-let XLSX = require('xlsx');
-const jsonwrite = require("jsonfile");
-let workbook = XLSX.readFile(`./.xlsx`);
-const excelContent = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]]);
+const xlsx = require("xlsx");
+const fs = require('fs');
+const jsonfile = require("jsonfile");
+const _ = require("lodash");
+let arrayFolder = ["followup", "medicine", "notification", "staging"];
+const { generateUid } = require("./utils");
+const data = require("./teiEnrMapping.json");
 
-
-let event = {};
-event["events"] = [];
-
-const generateEventsNghia = (data) => {
-    excelContent.forEach(exc => {
-
-        event["events"].push(createEventObject(exc,data));
-        createJsonFile();
-
-    });
-}
-
-
-
-const createJsonFile = () => {
-    jsonwrite.writeFileSync(`./AfterUpdate.json`, event);
-}
-
-const createEventObject = (exc, data) => {
-
+const createEventObject = (exc, data, eventId, ou, stagename) => {
+    
+    let config = require("./config.json");
+    let stagenameConfig = config[stagename];
+    
     let temp;
     if (data.hasOwnProperty(exc.PatientID)) {
         temp = {
-            "programStage": config[nameofExcelfile.split()].programStageId,
-            "orgUnit": nameofExcelfile.split(),
-            "program": "",
-            "trackedEntityInstance": data[exc.PatientID].trackedEntityInstance,
-            "enrollment": data[exc.PatientID].Enrollment,
+            "programStage": stagenameConfig.programStageId,
+            "orgUnit": ou,
+            "program": "ugLbPc9sYjQ",
+            "trackedEntityInstance": data[exc.PatientID].tei,
+            "enrollment": data[exc.PatientID].enr,
             "status": "COMPLETED",
-            "eventDate": "",
+            "eventDate": "2017-11-27T00:00:00.000",
             "followup": true,
             "dataValues": []
         }
 
-        for (z in exc) {
-            const dataval = {
-                "dataElement": config[nameofExcelfile.split()][z],
-                "value": exc[z]
-            };
-            temp.dataValues.push(dataval);
-        }
-
+        Object.keys(stagenameConfig).forEach(column => {
+            if (exc.hasOwnProperty(column)) {
+                temp.dataValues.push({
+                    dataElement: stagenameConfig[column],
+                    value: exc[column] + ""
+                });
+            }
+        });
+        
         return temp;
     }
 
 }
 
-modules.export = {
-    generateEventsNghia
+for (let i = 0; i <= 3; i++) {
+    let stageFileList = fs.readdirSync(`./input/${arrayFolder[i]}`);
+
+    stageFileList.forEach(stage => {
+        
+        
+        let workbook = xlsx.readFile(`./input/${arrayFolder[i]}/${stage}`);
+        let ou = stage.split("_")[0];
+        let stagename = stage.split("_")[1];
+        stagename = stagename.split(".")[0];
+        
+        let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        let stageList = xlsx.utils.sheet_to_json(worksheet);
+        let event = [];
+
+        
+        stageList.forEach(sl => {
+            let eventId = generateUid();
+            event.push(createEventObject(sl,data,eventId,ou,stagename));
+        })
+        event = _.chunk(event, 5000);
+        let eventIndex = 1;
+        event.forEach(list => {
+            jsonfile.writeFileSync(`./ouput/${arrayFolder[i]}/${stage}_output_${eventIndex}.json`, { events: list });
+            eventIndex += 1;
+        });
+
+    });
 }
+
+
