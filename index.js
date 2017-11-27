885878142bfb412f8123a9a12ddf67d64c95eb1d
Dung <nguyenanhdung191@gmail.com>
2017-11-27 09:42:38 +0700

    dung push

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 const { generateTeiEnrEvent} = require("./tei-utils");
+const {generate4StagesEvent}= require("./program-stage-utils");
 generateTeiEnrEvent();
+generate4StagesEvent();
--- program-stage-utils.js
+++ program-stage-utils.js
@@ -7,10 +7,10 @@ const { generateUid } = require("./utils");
 const data = require("./teiEnrMapping.json");
 
 const createEventObject = (exc, data, eventId, ou, stagename) => {
-    
+
     let config = require("./config.json");
     let stagenameConfig = config[stagename];
-    
+
     let temp;
     if (data.hasOwnProperty(exc.PatientID)) {
         temp = {
@@ -33,40 +33,44 @@ const createEventObject = (exc, data, eventId, ou, stagename) => {
                 });
             }
         });
-        
+
         return temp;
     }
 
 }
 
-for (let i = 0; i <= 3; i++) {
-    let stageFileList = fs.readdirSync(`./input/${arrayFolder[i]}`);
 
-    stageFileList.forEach(stage => {
-        
-        
-        let workbook = xlsx.readFile(`./input/${arrayFolder[i]}/${stage}`);
-        let ou = stage.split("_")[0];
-        let stagename = stage.split("_")[1];
-        stagename = stagename.split(".")[0];
-        
-        let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        let stageList = xlsx.utils.sheet_to_json(worksheet);
-        let event = [];
+const generate4StagesEvent = () => {
+    for (let i = 1; i <= 3; i++) {
+        let stageFileList = fs.readdirSync(`./input/${arrayFolder[i]}`);
 
-        
-        stageList.forEach(sl => {
-            let eventId = generateUid();
-            event.push(createEventObject(sl,data,eventId,ou,stagename));
-        })
-        event = _.chunk(event, 5000);
-        let eventIndex = 1;
-        event.forEach(list => {
-            jsonfile.writeFileSync(`./ouput/${arrayFolder[i]}/${stage}_output_${eventIndex}.json`, { events: list });
-            eventIndex += 1;
+        stageFileList.forEach(stage => {
+            let workbook = xlsx.readFile(`./input/${arrayFolder[i]}/${stage}`);
+            let ou = stage.split("_")[0];
+            let stagename = stage.split("_")[1];
+            stagename = stagename.split(".")[0];
+
+            let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+            let stageList = xlsx.utils.sheet_to_json(worksheet);
+            let event = [];
+
+            stageList.forEach(sl => {
+                let eventId = generateUid();
+                event.push(createEventObject(sl, data, eventId, ou, stagename));
+            })
+            event = _.chunk(event, 5000);
+            let eventIndex = 1;
+            event.forEach(list => {
+                jsonfile.writeFileSync(`./output/${arrayFolder[i]}/${stage}_output_${eventIndex}.json`, { events: list });
+                eventIndex += 1;
+            });
+
         });
+    }
+};
 
-    });
+module.exports = {
+    generate4StagesEvent
 }
 
 
